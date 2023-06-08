da363e2b1f5885ce1fc8c17b066b9f77418cbeef
Gustav Carlberg <55480495+Litene@users.noreply.github.com>
2023-06-08 23:12:16 +0200

    qwd
    qdwq

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +9,16 @@ public class CharacterMovement : MonoBehaviour {
     // Start is called before the first frame update
     public CharacterController _characterController;
     public Transform _cam;
+    private Rigidbody _rb;
 
     public float Speed = 30.0f;
     public float TurnSpeed = 0.1f;
     private float _turnSpeedVelocity;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     private void Update() {
         float Vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(0, 0, Vertical);
@@ -21,7 +28,15 @@ public class CharacterMovement : MonoBehaviour {
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSpeedVelocity, Speed);
             transform.rotation = Quaternion.Euler(0, angle, 0);
             Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            _characterController.Move(moveDir.normalized * Speed * Time.deltaTime);
-        
+            _characterController.Move(moveDir.normalized + Gravity() * Speed * Time.deltaTime);
+            
+    }
+
+    private Vector3 Gravity() {
+        return new Vector3(0, -0.3f, 0);
+    }
+    
+    private void FixedUpdate() {
+        Gravity();
     }
 }
