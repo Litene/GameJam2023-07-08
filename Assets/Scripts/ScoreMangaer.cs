d4909f77e359fda8c543de29ff8bfcc5b490575e
Gustav Carlberg <55480495+Litene@users.noreply.github.com>
2023-06-08 23:35:58 +0200

    qwd
    dqd

--- Assets/Scripts/ScoreMangaer.cs
+++ Assets/Scripts/ScoreMangaer.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine;
 
 public class ScoreMangaer : MonoBehaviour {
-    private TextMeshProUGUI _text;
+    public TextMeshProUGUI _text;
     public Character _char;
     private void Update() {
         _text.text = _char.Size.ToString();
